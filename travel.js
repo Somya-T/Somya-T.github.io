1d04759d3c47cbf6059d354650618a02f446127b
Somya-T <40634502+Somya-T@users.noreply.github.com>
2018-06-27 09:54:45 -0400

    Add files via upload

---
+++ travel.js
@@ -0,0 +1,20 @@
+
+function myFunction() {
+    alert("What season lover are you? By finding out, you'll be able to find the perfect destination that fits you! Click the right boxes to the right that stands out to you the most!");
+}
+
+function myFunction2() {
+    alert("SUMMER LOVER! Do you enjoy the heat? If so, Dubai would be great! If hot places really bother you then Switzerland should be your location!! ")
+}
+
+function myFunction3() {
+    alert("FALL LOVER! Do you love the site of hundreds of shades of colors on trees while driving up the mountains? If so, Gatlinburg should be your destination!!")
+}
+
+function myFunction4() {
+    alert("WINTER LOVER!! Do you just love the Christmas feeling? There's no better place than New York City to visit in the winter time!")
+}
+
+function myFunction5() {
+    alert("SPRING LOVER! Do you enjoy seeing the sights of cherry blossom trees in the capitol? If so, Washington D.C. should be the destination for you! If you love botanical gardens, Montreal should be your location!")
+}
